744e53671a1cfc9288f012e7a2b1c4838149a0d3
Aquinter10 <124784123+Aquinter10@users.noreply.github.com>
2025-07-31 14:25:50 -0500

    Activity 2, 3, 4, 5

--- helloworld/pages/urls.py
+++ helloworld/pages/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path 
-from .views import HomePageView, ContactPageView, AboutPageView
+from .views import HomePageView, ContactPageView, AboutPageView, ProductIndexView, ProductShowView
 
  
 urlpatterns = [ 
 path("", HomePageView.as_view(), name='home'),
      path("contact/", ContactPageView.as_view(), name='contact'),
-     path('about/', AboutPageView.as_view(), name='about')
+     path('about/', AboutPageView.as_view(), name='about'),
+     path('products/', ProductIndexView.as_view(), name='index'), 
+     path('products/<str:id>', ProductShowView.as_view(), name='show'),
   ]
--- helloworld/pages/views.py
+++ helloworld/pages/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView  
-
+from django.views import View 
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 class HomePageView(TemplateView):
      template_name = 'pages/home.html'
 
@@ -21,3 +23,44 @@ class AboutPageView(TemplateView):
         }) 
  
         return context 
+    
+ 
+class Product: 
+    products = [ 
+        {"id":"1", "name":"TV", "description":"Best TV","price": 1000}, 
+        {"id":"2", "name":"iPhone", "description":"Best iPhone","price": 2000}, 
+        {"id":"3", "name":"Chromecast", "description":"Best Chromecast","price": 1500}, 
+        {"id":"4", "name":"Glasses", "description":"Best Glasses","price": 5000} 
+    ] 
+ 
+class ProductIndexView(View): 
+    template_name = 'products/index.html' 
+ 
+    def get(self, request): 
+        viewData = {} 
+        viewData["title"] = "Products - Online Store" 
+        viewData["subtitle"] =  "List of products" 
+        viewData["products"] = Product.products 
+ 
+        return render(request, self.template_name, viewData) 
+ 
+class ProductShowView(View): 
+    template_name = 'products/show.html' 
+ 
+    def get(self, request, id): 
+        try:
+            product_index = int(id) - 1
+            if product_index < 0 or product_index >= len(Product.products):
+                raise IndexError
+            product = Product.products[product_index]
+        except (ValueError, IndexError):
+            return HttpResponseRedirect(reverse('home'))
+
+        viewData = {
+            "title": product["name"] + " - Online Store",
+            "subtitle": product["name"] + " - Product information",
+            "product": product,
+            "price": str(product["price"]) + " $ USD"
+        }
+
+        return render(request, self.template_name, viewData)
